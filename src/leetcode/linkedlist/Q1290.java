7a125c2aa35fc87f30e7c800a642164c846ddfee
smpark1020 <smpark911020@gmail.com>
2021-02-28 13:15:06 +0900

    Convert Binary Number in a Linked List to Integer

---
+++ src/leetcode/linkedlist/Q1290.java
@@ -0,0 +1,68 @@
+package leetcode.linkedlist;
+
+public class Q1290 {
+	
+	public static void main(String[] args) {
+		Q1290 q = new Q1290();
+		ListNode head = new ListNode(0);
+		head.next = new ListNode(0);
+//		head.next.next = new ListNode(0);
+//		head.next.next.next = new ListNode(0);
+		System.out.println(q.getDecimalValue_other(head));
+	}
+
+	public int getDecimalValue(ListNode head) {
+		int size = 0;
+		ListNode currentNode = head;
+		while (currentNode != null) {
+			size++;
+			currentNode = currentNode.next;
+		}
+		
+		int result = 0;
+		currentNode = head;
+		while (currentNode != null) {
+			size--;
+			if (currentNode.val == 1) {
+				result += Math.pow(2, size);
+			}
+			
+			currentNode = currentNode.next;
+		}
+		
+		return result;
+	}
+	
+	public int getDecimalValue_other(ListNode head) {
+		ListNode currentNode = head;
+		StringBuilder sb = new StringBuilder();
+		while (currentNode != null) {
+			sb.append(currentNode.val);
+			currentNode = currentNode.next;
+		}
+		
+		return Integer.parseInt(sb.toString(), 2);
+	}
+	
+}
+
+class ListNode {
+	
+    int val;
+    ListNode next;
+    
+    ListNode() {
+    	
+    }
+    
+    ListNode(int val) {
+    	this.val = val;
+    }
+    
+    ListNode(int val, ListNode next) {
+    	this.val = val;
+    	this.next = next;
+    }
+    
+}
+ 
