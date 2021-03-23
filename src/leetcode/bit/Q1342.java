68fbf3d74fb41b8f1c945e8a3b2546515cb68379
smpark1020 <smpark911020@gmail.com>
2021-03-23 13:39:41 +0900

    Number of Steps to Reduce a Number to Zero

---
+++ src/leetcode/bit/Q1342.java
@@ -0,0 +1,44 @@
+package leetcode.bit;
+
+public class Q1342 {
+	
+	public static void main(String[] args) {
+		Q1342 q = new Q1342();
+		System.out.println(q.numberOfSteps(14));
+		System.out.println(q.numberOfSteps(8));
+		
+		System.out.println(q.numberOfSteps_other(14));
+		System.out.println(q.numberOfSteps_other(8));
+	}
+	
+	public int numberOfSteps (int num) {
+		int step = 0;
+        while (num > 0) {
+        	if (num % 2 == 0) {
+        		num >>= 1;
+        	} else {
+        		num -= 1;
+        	}
+        	
+        	step++;
+        }
+        
+        return step;
+    }
+	
+	public int numberOfSteps_other(int num) {
+		int step = 0;
+        while (num > 0) {
+        	if ((num & 1) == 1) {
+        		num >>= 1;
+        	} else {
+        		num -= 1;
+        	}
+        	
+        	step++;
+        }
+        
+        return step;
+    }
+	
+}
