0780ccc1ff6974ed8d2c5773746604e7603e8d4c
smpark1020 <smpark911020@gmail.com>
2021-05-05 14:50:52 +0900

    Top K Frequent Elements

---
+++ src/leetcode/heap/Q347.java
@@ -0,0 +1,58 @@
+package leetcode.heap;
+
+import java.util.*;
+
+/**
+ * LeetCode
+ * 347. Top K Frequent Elements
+ */
+public class Q347 {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        List<NumCount> list = new ArrayList<>();
+        for (int num : map.keySet()) {
+            list.add(new NumCount(num, map.get(num)));
+        }
+
+        PriorityQueue<NumCount> queue = new PriorityQueue<>((a, b) -> {
+           return b.count - a.count;
+        });
+
+        for (NumCount object : list) {
+            queue.offer(object);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        while (!queue.isEmpty() && k-- > 0) {
+            NumCount numCount = queue.poll();
+            result.add(numCount.num);
+        }
+
+        int[] resultArray = new int[result.size()];
+        for (int i = 0; i < resultArray.length; i++) {
+            resultArray[i] = result.get(i);
+        }
+        return resultArray;
+    }
+
+    static class NumCount {
+
+        int num;
+        int count;
+
+        public NumCount(int num, int count) {
+            this.num = num;
+            this.count = count;
+        }
+    }
+
+}
