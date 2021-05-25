87cff8fac88a3ff60336b97f63600cae2af64f81
smpark1020 <smpark911020@gmail.com>
2021-05-25 13:49:54 +0900

    Check If Two String Arrays are Equivalent

---
+++ src/leetcode/string/Q1662.java
@@ -0,0 +1,23 @@
+package leetcode.string;
+
+/**
+ * LeetCode
+ * 1662. Check If Two String Arrays are Equivalent
+ */
+public class Q1662 {
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        for (String w : word1) {
+            sb1.append(w);
+        }
+
+        StringBuilder sb2 = new StringBuilder();
+        for (String w : word2) {
+            sb2.append(w);
+        }
+
+        return sb1.toString().equals(sb2.toString());
+    }
+
+}
