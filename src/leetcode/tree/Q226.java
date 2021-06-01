496224f6bc4ad30456f6e1e61437d69b279fb27d
smpark1020 <smpark911020@gmail.com>
2021-06-01 13:16:09 +0900

    Invert Binary Tree

---
+++ src/leetcode/tree/Q226.java
@@ -0,0 +1,58 @@
+package leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * LeetCode
+ * 226. Invert Binary Tree
+ */
+public class Q226 {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+
+            if (node.left != null || node.right != null) {
+                TreeNode temp = node.left;
+                node.left = node.right;
+                node.right = temp;
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) { this.val = val; }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
