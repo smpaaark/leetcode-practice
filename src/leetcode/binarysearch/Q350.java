72ee04500c5b4a2b527f977996e5f63af7cfbc9c
smpark1020 <smpark911020@gmail.com>
2021-06-08 09:40:18 +0900

    Intersection of Two Arrays II

---
+++ src/leetcode/binarysearch/Q350.java
@@ -0,0 +1,31 @@
+package leetcode.binarysearch;
+
+import java.util.*;
+
+/**
+ * LeetCode
+ * 350. Intersection of Two Arrays II
+ */
+public class Q350 {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                }
+            }
+        }
+
+        return list.stream().mapToInt(i->i).toArray();
+    }
+
+}
