7e7fdc523a80eaf39d4f1661b36fa68a10e5c6fe
smpark1020 <smpark911020@gmail.com>
2021-06-19 14:52:55 +0900

    Count Good Triplets

---
+++ src/leetcode/array/Q1534.java
@@ -0,0 +1,26 @@
+package leetcode.array;
+
+/**
+ * LeetCode
+ * 1534. Count Good Triplets
+ */
+public class Q1534 {
+
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int result = 0;
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (Math.abs(arr[i] - arr[j]) <= a) {
+                    for (int k = j + 1; k < arr.length; k++) {
+                        if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                            result++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
