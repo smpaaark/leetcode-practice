17dbb2603fa36e2ea89575c46b48939419b0fc5f
smpark1020 <smpark911020@gmail.com>
2021-07-08 09:44:23 +0900

    Plus One

---
+++ src/leetcode/interview/Q66.java
@@ -0,0 +1,25 @@
+package leetcode.interview;
+
+/**
+ * LeetCode
+ * 66. Plus One
+ */
+public class Q66 {
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+
+}
