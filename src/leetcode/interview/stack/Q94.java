30faac2ea56c18bc9e66c2300d040a91f18064e8
smpark1020 <smpark911020@gmail.com>
2021-07-13 13:47:30 +0900

    주석 추가

--- src/leetcode/interview/stack/Q94.java
+++ src/leetcode/interview/stack/Q94.java
@@ -16,7 +16,7 @@ public class Q94 {
 
         Stack<TreeNode> stack = new Stack<>();
         TreeNode currentNode = root; // 현재 노드
-        while (currentNode != null || !stack.isEmpty()) {
+        while (currentNode != null || !stack.isEmpty()) { // !stack.isEmpty는 오른쪽 노드가 null일 경우 stack에 남아있는 노드로 계속 진행해야 되기 때문에 조건이 있어야 한다.
             while (currentNode != null) { // 왼쪽 노드를 stack에 push 한다.
                 stack.push(currentNode);
                 currentNode = currentNode.left;
@@ -24,7 +24,7 @@ public class Q94 {
 
             currentNode = stack.pop();
             list.add(currentNode.val); // stack에서 꺼내서 list에 add
-            currentNode = currentNode.right; // 오른쪽 노드가 있을 경우 위 과정 반복
+            currentNode = currentNode.right; // 오른쪽 노드로 위 과정 반복
         }
 
         return list;
